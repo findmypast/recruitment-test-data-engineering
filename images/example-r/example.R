474b8ed1b6ac7b3117afe94c4935dce0460a51cc
Andrew J Patterson <workonly@qidane.com>
2020-06-30 17:58:17 +0100

    Working example code in R. Cannot promise it is idiomatic, but it works. Paired with Toby.

--- images/example-r/example.R
+++ images/example-r/example.R
@@ -6,17 +6,14 @@ library(DBI)
 
 # connect to the database
 database <- dbConnect(RMySQL::MySQL(), user = 'codetest', password = 'swordfish', dbname = 'codetest', host = 'database')
-dbListTables(database)
 
 # read the CSV data file into the table
 example_data <- read_csv('/data/example.csv')
-# TODO: write the data into the database
-dbSendQuery(database, "insert into examples(name) values('Testie')")
+apply(example_data, c(1, 2), function(x) dbSendQuery(database, paste0("insert into examples(name) values('", x, "')")))
 
 # output the table to a JSON file
 res <- dbSendQuery(database, "select * from examples")
 output = dbFetch(res)
 dbClearResult(res)
 
-write_json(output, '/data/example_r.json', pretty = FALSE, auto_unbox = TRUE)
-
+write_json(output, '/data/example_r.json', pretty = FALSE)

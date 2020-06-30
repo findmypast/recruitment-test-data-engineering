a42497bf69403b206b108e517a05afa32363082d
Toby Atkin-Wright <tobyaw@me.com>
2020-06-30 16:47:21 +0100

    Added sample db insert, and proper querying for the JSON output. Still work-in-progress. Paired w/ Andrew.

--- images/example-r/example.R
+++ images/example-r/example.R
@@ -2,15 +2,21 @@
 
 library(tidyverse)
 library(jsonlite)
+library(DBI)
 
 # connect to the database
-# ???
+database <- dbConnect(RMySQL::MySQL(), user = 'codetest', password = 'swordfish', dbname = 'codetest', host = 'database')
+dbListTables(database)
 
 # read the CSV data file into the table
 example_data <- read_csv('/data/example.csv')
-
-# debug output
-print(example_data)
+# TODO: write the data into the database
+dbSendQuery(database, "insert into examples(name) values('Testie')")
 
 # output the table to a JSON file
-write_json(example_data, '/data/example_r.json', pretty = FALSE, auto_unbox = TRUE)
+res <- dbSendQuery(database, "select * from examples")
+output = dbFetch(res)
+dbClearResult(res)
+
+write_json(output, '/data/example_r.json', pretty = FALSE, auto_unbox = TRUE)
+

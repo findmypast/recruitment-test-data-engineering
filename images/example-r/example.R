aec0fca1000871aa33f8024b0f362a041fbfd48d
Toby Atkin-Wright <tobyaw@me.com>
2020-06-30 14:28:33 +0100

    Writing some debugging output.

--- images/example-r/example.R
+++ images/example-r/example.R
@@ -3,6 +3,14 @@
 library(tidyverse)
 library(jsonlite)
 
+# connect to the database
+# ???
+
+# read the CSV data file into the table
 example_data <- read_csv('/data/example.csv')
 
-write_json(example_data, '/data/example_r.json', pretty = TRUE, auto_unbox = TRUE)
+# debug output
+print(example_data)
+
+# output the table to a JSON file
+write_json(example_data, '/data/example_r.json', pretty = FALSE, auto_unbox = TRUE)

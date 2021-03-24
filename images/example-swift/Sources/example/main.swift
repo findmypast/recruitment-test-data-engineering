17d02c324cd00a7c64fb142c9e103b316c85453b
Toby Atkin-Wright <tobyaw@me.com>
2021-03-24 09:47:21 +0000

    Simplifying code, trying some different error handling.

--- images/example-swift/Sources/example/main.swift
+++ images/example-swift/Sources/example/main.swift
@@ -15,29 +15,27 @@ struct Example : Model {
 }
 
 let semaphore = DispatchSemaphore(value: 0)
-let file = try? String(contentsOf: URL(fileURLWithPath: "/data/example.csv"))
-if let text = file {
-  for name in text.trimmingCharacters(in: .whitespacesAndNewlines).components(separatedBy: "\n").dropFirst(1) {
-    Example(name: name).save { example, error in
-      semaphore.signal()
-    }
-    semaphore.wait()
-  }
+
+for name in try! String(contentsOf: URL(fileURLWithPath: "/data/example.csv"))
+                  .trimmingCharacters(in: .whitespacesAndNewlines)
+                  .components(separatedBy: "\n").dropFirst(1) {
+  Example(name: name).save { _, _ in semaphore.signal()}
+  semaphore.wait()
 }
 
 Example.findAll { (result: [(Int, Example)]?, error: RequestError?) in
-  if let rows = result {
-    let json = "[" + rows.sorted {
-      $0.0 < $1.0
-    }.map { item in
-      return "{\"id\":\(item.0),\"name\":\"\(item.1.name)\"}"
-    }.joined(separator: ",") + "]"
+  guard result != nil else {
+    print(error!)
+    semaphore.signal()
+    return
+  }
 
-    do {
-      try json.write(to: URL(fileURLWithPath: "/data/example_swift.json"), atomically: true, encoding: .utf8)
-    } catch {
-      print(error)
-    }
+  if let rows = result {
+    let text = rows.sorted {$0.0 < $1.0}
+                   .map { item in return "{\"id\":\(item.0),\"name\":\"\(item.1.name)\"}"}
+                   .joined(separator: ",")
+
+    try! "[\(text)]".write(to: URL(fileURLWithPath: "/data/example_swift.json"), atomically: true, encoding: .utf8)
   }
 
   semaphore.signal()

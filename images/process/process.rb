3620aadac5ebbdb7fbd70f16765af84de25ee697
Albert Goody <albert.goody@gmail.com>
2022-07-12 18:38:51 +0100

    corrected code

--- images/process/process.rb
+++ images/process/process.rb
@@ -4,7 +4,7 @@ require 'active_record'
 require 'csv'
 
 class Place < ActiveRecord::Base
-  has_many :person
+  has_many :people
 end
 
 class Person < ActiveRecord::Base
@@ -25,13 +25,12 @@ Place.destroy_all
 
 CSV.read('/data/places.csv', headers: true).map(&:to_h).each do |row|
   Place.create(row)
-
 end
 
 CSV.read('/data/people.csv', headers: true).map(&:to_h).each do |row|
-  row.merge!(place_id: Place.find_by(city: row['place_of_birth']).id)
-
-  Person.create(row)
+  person = Person.create(row)
+  person.place = Place.find_by(city: person.place_of_birth)
+  person.save!
 end
 
 json_data = Person.all.map(&:place).group_by(&:country).transform_values(&:count)
--- schema.sql
+++ schema.sql
@@ -1,18 +1,18 @@
-create table `person` (
-  `id` int not null auto_increment,
-  `given_name` varchar(80) default null,
-  `family_name` varchar(80) default null,
-  `date_of_birth` Date default null,
-   `place_of_birth` varchar(80) default null,
-   'place_id' bigint
-  primary key (`id`)
-
-);
-
 create table `places` (
   `id` int not null auto_increment,
   `city` varchar(80) default null,
   `county` varchar(80) default null,
   `country` varchar(80) default null,
   primary key (`id`)
+);
+
+create table `people` (
+  `id` int not null auto_increment,
+  `given_name` varchar(80) default null,
+  `family_name` varchar(80) default null,
+  `date_of_birth` Date default null,
+   `place_of_birth` varchar(80) default null,
+   `place_id` int,
+   foreign key (`place_id`) references places(id),
+    primary key (`id`)
 );
